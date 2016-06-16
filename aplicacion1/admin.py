025128f17e15de46db0314a98fd1e4a34a831d29
jmcarne <josep.mariacarne@amaris.com>
2016-06-16 13:14:13 +0200

    Demo-python

--- aplicacion1/admin.py
+++ aplicacion1/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from aplicacion1.models import Jvconsultacontroller
+from aplicacion1.models import Jvconsultacontroller, Sensor
 
 
 class JvconsultacontrollerAdmin (admin.ModelAdmin):
     pass
 admin.site.register(Jvconsultacontroller,JvconsultacontrollerAdmin)
+
+
+class SensorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Sensor, SensorAdmin)
